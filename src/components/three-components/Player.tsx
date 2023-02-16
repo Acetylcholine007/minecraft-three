984491b410a6abedf4c53fb85c6e5664af98803d
Rahino Quijano <rahinoquijano.xurpas@gmail.com>
2023-02-16 15:31:22 +0800

    feat: add movement and jump controls to player

--- src/components/three-components/Player.tsx
+++ src/components/three-components/Player.tsx
@@ -1,3 +1,4 @@
+import { JUMP_FORCE, MOVE_SPEED } from '@/constants/player';
 import useKeyboard from '@/hooks/useKeyboard';
 import { useSphere } from '@react-three/cannon';
 import { useFrame, useThree } from '@react-three/fiber';
@@ -7,7 +8,8 @@ import { BufferGeometry, Mesh, Vector3 } from 'three';
 export interface PlayerProps {}
 
 const Player: React.FC<PlayerProps> = () => {
-  const actions = useKeyboard();
+  const { moveBackward, moveForward, moveLeft, moveRight, jump } =
+    useKeyboard();
 
   const { camera } = useThree();
   const [ref, api] = useSphere(() => ({
@@ -33,6 +35,30 @@ const Player: React.FC<PlayerProps> = () => {
 
   useFrame(() => {
     camera.position.copy(new Vector3(...position.current));
+
+    const direction = new Vector3();
+    const frontVector = new Vector3(
+      0,
+      0,
+      (moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+    );
+    const sideVector = new Vector3(
+      (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+      0,
+      0
+    );
+
+    direction
+      .subVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(MOVE_SPEED)
+      .applyEuler(camera.rotation);
+
+    api.velocity.set(direction.x, velocity.current[1], direction.z);
+
+    if (jump && Math.abs(velocity.current[1]) < 0.05) {
+      api.velocity.set(velocity.current[0], JUMP_FORCE, velocity.current[2]);
+    }
   });
 
   return <mesh ref={ref as React.RefObject<Mesh<BufferGeometry>>}></mesh>;
