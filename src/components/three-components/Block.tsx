3556a5c52aba88e7de05819d658e118c78950c7c
Rahino Quijano <rahinoquijano.xurpas@gmail.com>
2023-02-16 23:56:10 +0800

    feat: add block hover indicator and made glass translucent

--- src/components/three-components/Block.tsx
+++ src/components/three-components/Block.tsx
@@ -2,7 +2,7 @@ import { textureImages, textureSelector } from '@/assets/textures';
 import useStore from '@/hooks/useStore';
 import { useBox } from '@react-three/cannon';
 import { ThreeEvent } from '@react-three/fiber';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { BufferGeometry, Mesh } from 'three';
 
 export interface BlockProps {
@@ -12,6 +12,7 @@ export interface BlockProps {
 
 const Block: React.FC<BlockProps> = (props) => {
   const { position, texture } = props;
+  const [isHovered, setIsHovered] = useState(false);
   const [addBlock, removeBlock] = useStore((state) => [
     state.addBlock,
     state.removeBlock,
@@ -55,16 +56,37 @@ const Block: React.FC<BlockProps> = (props) => {
           return;
       }
     },
-    [addBlock]
+    [ref, addBlock, removeBlock]
   );
+
+  const onPointerMoveHandler = useCallback(
+    (event: ThreeEvent<PointerEvent>) => {
+      event.stopPropagation();
+      setIsHovered(true);
+    },
+    []
+  );
+
+  const onPointerOutHandler = useCallback((event: ThreeEvent<PointerEvent>) => {
+    event.stopPropagation();
+    setIsHovered(false);
+  }, []);
 
   return (
     <mesh
       onClick={meshClickHandler}
+      onPointerMove={onPointerMoveHandler}
+      onPointerOut={onPointerOutHandler}
       ref={ref as React.RefObject<Mesh<BufferGeometry>>}
     >
       <boxBufferGeometry attach="geometry" />
-      <meshStandardMaterial attach="material" map={activeTexture} />
+      <meshStandardMaterial
+        attach="material"
+        map={activeTexture}
+        color={isHovered ? 'grey' : 'white'}
+        transparent={texture === 'glass'}
+        opacity={texture === 'glass' ? 0.6 : 1}
+      />
     </mesh>
   );
 };
